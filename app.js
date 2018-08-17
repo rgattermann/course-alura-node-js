2ef43ece31b1942683e5758b76788365f83e02a6
Rodrigo Luciano Gattermann <rodrigo.gattermann@topaziopartner.com.br>
2018-08-17 14:26:30 -0300

    Update main file

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
-var express = require('express');
-var app = express();
-
-app.set('view engine', 'ejs');
-
-app.get('/products', function (req, res) {
-    res.render('products/list');
-});
+var app = require('./config/express')();
+var routes = require('./app/routes/products')(app);
 
 app.listen(3000, function() {
     console.log("It's alive!");
