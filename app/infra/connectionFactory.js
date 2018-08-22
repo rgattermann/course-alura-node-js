930012c4546a99f0763fc092c046d04a1c10ddad
Rodrigo Luciano Gattermann <rodrigo.gattermann@topaziopartner.com.br>
2018-08-22 17:00:52 -0300

    Creating factory connection database

---
+++ app/infra/connectionFactory.js
@@ -0,0 +1,8 @@
+var mysql = require('mysql');
+module.exports = function() {
+    return mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'casadocodigo'
+};
--- app/routes/products.js
+++ app/routes/products.js
@@ -1,12 +1,8 @@
+var connectionFactory = require('../infra/connectionFactory');
+
 module.exports = function (app) {
     app.get('/products', function (req, res) {
-        var mysql = require('mysql');
-        var connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'casadocodigo'
-        });
+        var connection = connectionFactory();
 
         connection.query('SELECT * FROM livros', function(err, result) {
             res.render('products/list', {lista: result});
